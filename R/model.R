8a136ac341f6fa0f9891edc403412b82f0750197
Corentin Lemasson <corentin.lemasson@nbb.be>
2026-06-03 10:59:59 +0200

    refactoring function documentation for CRAN

---
+++ R/model.R
@@ -0,0 +1,117 @@
+#' @include utils.R
+NULL
+
+#' @title Create a Dynamic Factor Model
+#'
+#' @description
+#' Creates a Dynamic Factor Model (DFM) by specifying the number of factors and
+#' the number of lags in the Vector Auto-Regressive (VAR) process. The
+#' function also allows the user to choose between different types of links
+#' between each series and the latent factors, in addition to defining the
+#' factor loading structure.
+#'
+#' @param nfactors Integer. Number of factors.
+#' @param nlags Integer. Number of lags in the VAR process.
+#' @param factors_type Character vector. Defines, in the order of the input series, how each (transformed) series relates to the factors.
+#' Available options are:
+#' \itemize{
+#'    \item{"M"}{: Variables expressed as monthly growth rates, linked directly to the latent factors.}
+#'    \item{"Q"}{: Quarterly or monthly variables linked to a weighted average of the factors, representing quarterly growth rates.}
+#'    \item{"YoY"}{: Variables linked to the cumulative sum of the last 12 monthly factors, corresponding to year-on-year growth rates. This option is appropriate for variables expressed in year-on-year terms, or for series that are closely related to such evolution.}
+#' }
+#' @param factors_loading Boolean matrix defining the factor loading structure, with dimension \emph{number of series} × \emph{number of factors}. Each entry indicates whether a given factor loads on a given series.
+#' @param var_init Character. Specifies the initialization of the latent factors: either zero or assumed to follow a normal distribution with mean zero and variance equal to the unconditional variance of the VAR (default).
+#' @param var_coefficients Matrix. The VAR coefficients. If `NULL` (default), they will be estimated from scratch.
+#' Otherwise, user-defined values can be provided as starting point for the estimation, typically obtained from a previous estimation of the model.
+#' @param var_errors_variance Matrix. The variance-covariance matrix of the VAR errors. If `NULL` (default), it will be estimated from scratch.
+#' Otherwise, user-defined values can be provided as starting point for the estimation, typically obtained from a previous estimation of the model.
+#' @param measurement_coefficients Matrix. The measurement equation coefficients. If `NULL` (default), they will be estimated from scratch.
+#' Otherwise, user-defined values can be provided as starting point for the estimation, typically obtained from a previous estimation of the model.
+#' @param measurement_errors_variance Numeric vector. The variance of the idiosyncratic measurement errors. If `NULL` (default), they will be estimated from scratch.
+#' Otherwise, user-defined values can be provided as starting point for the estimation, typically obtained from a previous estimation of the model.
+#'
+#' @return An object of class `"JD3_DFMMODEL"` is returned. The following are
+#' returned invisibly as a list:
+#' * `var_coefficients` `[[1]]` initial values of the VAR coefficients;
+#' * `var_errors_variance` `[[2]]` initial values of the variance-covariance matrix of the VAR errors;
+#' * `measurement_coefficients` `[[3]]` initial values of the measurement equation coefficients;
+#' * `measurement_errors_variance` `[[4]]` initial values of the variance of the idiosyncratic measurement errors;
+#' * `initialization_type` `[[5]]` the value of the `var_init` argument;
+#' * `factors_type` `[[6]]` the value of the `factors_type` argument.
+#'
+#' @export
+#'
+#' @seealso `estimate_pca()` to estimate the generated model using principal components analysis,
+#'
+#' `estimate_em()` to estimate the generated model using the Expectations-Maximization algorithm,
+#'
+#' `estimate_ml()` to estimate the generated model using maximum likelihood.
+#'
+#'
+#' For more information, see the vignette:
+#'
+#' `utils::browseVignettes()`, e.g. `browseVignettes(package = "rjd3nowcasting")`
+#'
+#' @examplesIf rjd3toolkit::get_java_version() >= rjd3toolkit::minimal_java_version
+#' # From scratch
+#' dfm1 <- create_model(nfactors = 2,
+#'                      nlags = 2,
+#'                      factors_type = c("M", "M", "YoY", "M", "Q"),
+#'                      factors_loading = matrix(data = TRUE, 5, 2),
+#'                      var_init = "Unconditional")
+#'
+#' # From a previously estimated model
+#' set.seed(100)
+#' data <- ts(matrix(rnorm(500), 100, 5),
+#'            frequency = 12,
+#'            start = c(2010, 1))
+#' data[100, 1] <- data[99:100, 2] <- data[(1:100)[-seq(3, 100, 3)], 5] <- NA
+#' est1 <- estimate_em(dfm1, data)
+#' dfm2 <- create_model(nfactors = 2,
+#'                      nlags = 2,
+#'                      factors_type = c("M", "M", "YoY", "M", "Q"),
+#'                      factors_loading = matrix(data = TRUE, 5, 2),
+#'                      var_init = "Unconditional",
+#'                      var_coefficients = est1$dfm$var_coefficients,
+#'                      var_errors_variance = est1$dfm$var_errors_variance,
+#'                      measurement_coefficients = est1$dfm$measurement_coefficients,
+#'                      measurement_errors_variance = est1$dfm$measurement_errors_variance)
+#'
+create_model <- function(nfactors,
+                         nlags,
+                         factors_type,
+                         factors_loading,
+                         var_init = c("Unconditional", "Zero"),
+                         var_coefficients = NULL,
+                         var_errors_variance = NULL,
+                         measurement_coefficients = NULL,
+                         measurement_errors_variance = NULL) {
+
+    var_init <- match.arg(var_init)
+    jfactors_loading <- rjd3toolkit::.r2jd_matrix(factors_loading)
+    jvar_coefficients <- rjd3toolkit::.r2jd_matrix(var_coefficients)
+    jvar_errors_variance <- rjd3toolkit::.r2jd_matrix(var_errors_variance)
+    jmeasurement_coefficients <- rjd3toolkit::.r2jd_matrix(measurement_coefficients)
+    if (is.null(measurement_errors_variance)) {
+        jmeasurement_errors_variance <- .jnull("[D")
+    } else {
+        jmeasurement_errors_variance <- .jarray(as.numeric(measurement_errors_variance))
+    }
+
+    jmodel <- .jcall(
+        "jdplus/dfm/base/r/DynamicFactorModels",
+        "Ljdplus/dfm/base/core/DynamicFactorModel;",
+        "model",
+        as.integer(nfactors),
+        as.integer(nlags),
+        .jarray(as.character(factors_type)),
+        jfactors_loading,
+        .jnew("java/lang/String", as.character(var_init)),
+        jvar_coefficients,
+        jvar_errors_variance,
+        jmeasurement_coefficients,
+        jmeasurement_errors_variance
+    )
+
+    return(.jd2r_dfm(jmodel))
+}
---
+++ R/print.R
@@ -0,0 +1,221 @@
+#' @include utils.R
+#' @importFrom stats ts.plot start
+#' @importFrom graphics axis legend par barplot points lines
+#' @importFrom grDevices palette.colors colorRampPalette
+NULL
+
+#' @export
+print.JD3_DFMESTIMATES <- function(x, ...) {
+    print(list(
+        has_converged = toupper(x$has_converged),
+        log_likelihood = x$log_likelihood,
+        ...
+    ))
+}
+
+#' @export
+print.JD3_DFMRESULTS <- function(x, ...){
+
+    sample_mean_stdev <- round(x$preprocessing$sample_mean_stdev, 3)
+    param_mcoeff <- round(x$parameters$measurement_coefficients, 3)
+    param_mvar <- round(x$parameters$measurement_errors_variance, 3)
+    loadings <- cbind(sample_mean_stdev, param_mcoeff, param_mvar)
+    colnames(loadings) <- c("Sample mean",
+                            "Stdev",
+                            paste0("Coeff. ", colnames(param_mcoeff)),
+                            "Idiosyncratic variance")
+
+    print(list(
+        loadings = loadings,
+        VAR_model = round(x$parameters$var_coefficients, 3),
+        Innovative_variance = round(x$parameters$var_errors_variance, 3),
+        ...
+    ))
+}
+
+#' @export
+print.JD3_DFMFORECASTS <- function(x, ...){
+    print(list(forecasts_only = x$forecasts_only, ...))
+}
+
+#' @export
+plot.JD3_DFMFORECASTS <- function(x, series_name = NULL, ...){
+
+    fcst <- x$forecasts
+    fcst_stdev <- x$forecasts_stdev
+    fcst_only <- x$forecasts_only
+
+    if (is.null(series_name)) {
+        series_name <- colnames(fcst)[1]
+    }
+
+    if (series_name %in% colnames(fcst)) {
+        s <- fcst[, series_name]
+        s_lb <- s - 1.28 * fcst_stdev[, series_name]
+        s_ub <- s + 1.28 * fcst_stdev[, series_name]
+        sf <- fcst_only[, series_name]
+    } else {
+        stop("series name not found!")
+    }
+
+    stats::ts.plot(
+        s_lb,
+        s_ub,
+        s,
+        sf,
+        gpars = list(
+            main = series_name,
+            sub = "Forecasts with a 80% prediction interval",
+            xlab = "",
+            ylab = "",
+            lty = c(3, 3, 1, 1),
+            xaxt = "n",
+            type = "o",
+            pch = 20,
+            cex = 0.8,
+            las = 2,
+            col = c("orange", "orange", "black", "red"),
+            ...
+        )
+    )
+    graphics::axis(1,
+                   at = seq(stats::start(s)[1], stats::end(s)[1], by = 1),
+                   las = 2)
+    graphics::legend(
+        "topleft",
+        legend = c("series", "forecasts", "80% PI"),
+        col = c("black", "red", "orange"),
+        lty = c(1, 1, 3),
+        cex = 0.8
+    )
+}
+
+
+#' @export
+print.JD3_DFMNEWS <- function(x, ...){
+    summary(x)
+    print(list(forecasts = round(x$forecasts,3), ...))
+}
+
+
+#' @export
+summary.JD3_DFMNEWS <- function(object, ...) {
+    x <- object
+
+    nf <- ncol(x$forecasts)
+
+    base <- cbind(
+        x$impacts[, 1:2],
+        round(as.matrix(sapply(x$impacts[, 3:5], as.numeric)), 3)
+    )
+
+    # Extract weights and impacts
+    weights <- as.matrix(sapply(x$weights[, 6:ncol(x$weights), drop = FALSE], as.numeric))
+    weights <- rbind(weights, colSums(weights))
+    impacts <- as.matrix(sapply(x$impacts[, 6:ncol(x$impacts), drop = FALSE], as.numeric))
+
+    # Interleave weights and impacts
+    wi_mat <- matrix(NA, nrow = nrow(weights), ncol = 2 * nf)
+    wi_mat[, seq(1, 2 * nf, by = 2)] <- round(weights, 3)
+    wi_mat[, seq(2, 2 * nf, by = 2)] <- round(impacts, 3)
+
+    colnames(wi_mat) <- as.vector(rbind(
+        colnames(x$weights)[6:(5 + nf)],
+        colnames(x$impacts)[6:(5 + nf)]
+    ))
+
+    # Final summary
+    summary_news <- cbind(base, wi_mat)
+
+    print(
+        list(
+            target_series = x$target_series,
+            summary_news = summary_news,
+            ...
+        ),
+        row.names = FALSE
+    )
+}
+
+#' @export
+plot.JD3_DFMNEWS <- function(x, ...) {
+    oldpar <- graphics::par(no.readonly = TRUE)
+    on.exit(graphics::par(oldpar))
+
+    start_col <- 6
+    impacts <- x$impacts
+    fcsts <- x$forecasts
+
+    nr <- nrow(impacts)
+    nf <- ncol(fcsts)
+
+    # Extract impacts (excluding total row)
+    impacts_mat <- as.matrix(
+        sapply(impacts[-nr, start_col:(start_col + nf - 1)], as.numeric)
+    )
+    colnames(impacts_mat) <- colnames(fcsts)
+    rownames(impacts_mat) <- impacts[-nr, 1]
+
+    # Total impacts
+    total_impacts <- as.numeric(
+        impacts[nr, start_col:(start_col + nf - 1)]
+    )
+
+    # Colors
+    plot_colors <- if (nr <= 10) {
+        grDevices::palette.colors(nr - 1)
+    } else {
+        grDevices::colorRampPalette(grDevices::palette.colors(8))(nr - 1)
+    }
+
+    # Legend text
+    legend_text <- paste0(impacts[-nr, 1], " (", impacts[-nr, 2], ")")
+    legend_text <- c(legend_text, "Total impact")
+
+    graphics::par(mar = c(5.1, 4.1, 4.1, 16.1), xpd = TRUE)
+
+    # Handle positive and negative contributions separately
+    impacts_positive <- impacts_mat
+    impacts_negative <- impacts_mat
+
+    impacts_positive[impacts_positive < 0] <- 0
+    impacts_negative[impacts_negative > 0] <- 0
+
+    # Plotting range
+    impacts_range <- c(
+        min(colSums(impacts_negative)),
+        max(colSums(impacts_positive))
+    )
+
+    # Barplot (negative first)
+    bar_positions <- graphics::barplot(
+        impacts_negative,
+        ylim = impacts_range,
+        main = paste0("Impacts of news on forecast revisions (", x$target_series, ")"),
+        col = plot_colors,
+        las = 1,
+        ...
+    )
+
+    # Overlay positive values
+    graphics::barplot(
+        impacts_positive,
+        add = TRUE,
+        ylim = rev(impacts_range),
+        col =  plot_colors,
+        las = 1,
+    )
+
+    # Overlay total impact
+    graphics::points(bar_positions, total_impacts, pch = 3, col = "red", cex = 1)
+    graphics::lines(bar_positions, total_impacts, col = "red")
+
+    # Legend
+    graphics::legend(
+        "bottomright",
+        legend = legend_text,
+        inset = c(-0.6, 0),
+        col = c(plot_colors, "red"),
+        pch = c(rep(15, nr - 1), 3)
+    )
+}
